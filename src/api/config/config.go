8ce8db93c934679a8d7a9efe133a9b97f9fb1221
rubenbuelvas <rcbuelvasv@gmail.com>
2026-09-18 21:06:04 -0400

    Load config vars

--- src/api/config/config.go
+++ src/api/config/config.go
@@ -13,7 +13,6 @@ func LoadConfig() {
 		panic(err)
 	}
 
-	// Load the two variables from config.yaml
 	viper.BindEnv("YUL_ARRIVALS_URL", "YUL_ARRIVALS_URL")
 	viper.BindEnv("YUL_DEPARTURES_URL", "YUL_DEPARTURES_URL")
 }
